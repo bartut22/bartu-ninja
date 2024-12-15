c6a92afe344d572275f5633a79540d3a33419c65
avocado5182 <46830012+avocado5182@users.noreply.github.com>
2024-12-15 15:54:29 -0500

    added programatic loading sketch

--- src/app/homeHero.js
+++ src/app/homeHero.js
@@ -1,35 +1,43 @@
-import dynamic from 'next/dynamic'
-import Link from 'next/link'
-import { Josefin_Sans } from 'next/font/google'
-const josefin_sans = Josefin_Sans({ subsets: ['latin'] })
-import styles from './homeHero.module.css'
+"use client";
 
-const HeroSketchWrapper = dynamic(() => import('./sketch'), {
-  ssr: false,
-})
+import { useState, useEffect } from 'react';
+import dynamic from 'next/dynamic';
+import Link from 'next/link';
+import { Josefin_Sans } from 'next/font/google';
+const josefin_sans = Josefin_Sans({ subsets: ['latin'] });
+import styles from './homeHero.module.css';
+import Navbar from './navbar'; // Assuming you have a Navbar component
 
 export default function HomeHeroWrapper() {
-    return(
+    const [loadSketch, setLoadSketch] = useState(false);
+
+    const handleLoadSketch = () => {
+        setLoadSketch(true);
+    };
+
+    useEffect(() => {
+        handleLoadSketch();
+    }, []);
+
+    const HeroSketchWrapper = loadSketch ? dynamic(() => import('./sketch'), { ssr: false }) : null;
+
+    return (
         <div className={styles.hero}>
-          {/* <!--
-              background with moving shapes with my name as text in the front
-              maybe the moving shapes can react to mouse?
-              100vw, 100vh
-          -->
+            {/* <!--
+                background with moving shapes with my name as text in the front
+                maybe the moving shapes can react to mouse?
+                100vw, 100vh
+            -->*/}
 
-          <h1 class="heroTitle">Bartu</h1>
-          <a class="heroBtn" href="#projects" style="top:calc(50% - 3.5em)">Go to my projects</a>
-          <a class="heroBtn" href="notes" style="top:calc(50%)">See my class notes</a>
+            {HeroSketchWrapper && <HeroSketchWrapper />}
 
-          <div class="dummy"></div> */}
+            <Navbar onLinkClick={handleLoadSketch} />
 
-          <HeroSketchWrapper></HeroSketchWrapper>
-
-          <div className={styles.heroFg}>
-            <h1 className={`${styles.heroTitle} ${josefin_sans.className}`}>Bartu</h1>
-            <Link href="#projects" tabIndex={0} className={styles.heroBtn}>Go to my projects</Link>
-            <Link href="/notes" tabIndex={1} className={styles.heroBtn}>See my class notes</Link>
-          </div>
+            <div className={styles.heroFg}>
+                <h1 className={`${styles.heroTitle} ${josefin_sans.className}`}>Bartu</h1>
+                <Link href="#projects" tabIndex={0} className={styles.heroBtn} onClick={handleLoadSketch}>Go to my projects</Link>
+                <Link href="/notes" tabIndex={1} className={styles.heroBtn}>See my class notes</Link>
+            </div>
         </div>
-    )
+    );
 }
